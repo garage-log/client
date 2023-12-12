85091689f4028df2e6e4774206c162f6c9dff77a
alipek-lab <alipek@gmail.com>
2023-12-12 14:17:41 +0100

    login form oluşturuldu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Garage logo"
+      class="logo"
+      src="@/assets/logos.png"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
